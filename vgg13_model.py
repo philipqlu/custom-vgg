5f5c873c3c071fd636b5f708d3446020f84b1bdd
Philip <philip.lu@hotmail.ca>
2017-07-29 18:04:30 +0700

    model update

--- vgg13_model.py
+++ vgg13_model.py
@@ -21,12 +21,15 @@ def bias_variable(shape):
 
 def vgg(X):
     '''
-      Custom with dropout and less units in FC layers.
+    Architecture: 2 + 2 + 3 conv + 3 max pool + 2 FC + 1 FC + out
+    Dropout after pooling and FC
       X: float32 tensor containing the training examples
-      Returns:
+    returns:
       y_out: output layer
-      keep_prob: tensorflow float, default 1.0 means no drop
+      keep_prob: tensorflow float that we will feed back later
     '''
+
+    keep_prob = tf.placeholder(tf.float32)
     # conv1_1
 
     with tf.name_scope('conv1_1') as scope:
@@ -46,16 +49,17 @@ def vgg(X):
 
     # pool1
     pool1 = max_pool_2x2(conv1_2, name='pool1')
+    pool1_drop = tf.nn.dropout(pool1, keep_prob)
 
     # conv2_1
     with tf.name_scope('conv2_1') as scope:
         kernel = weight_variable([3, 3, 32, 64])
-        conv = conv2d(pool1, kernel)
+        conv = conv2d(pool1_drop, kernel)
         biases = weight_variable([64])
         out = tf.nn.bias_add(conv, biases)
         conv2_1 = tf.nn.relu(out, name=scope)
 
-        # conv2_2
+    # conv2_2
     with tf.name_scope('conv2_2') as scope:
         kernel = weight_variable([3, 3, 64, 64])
         conv = conv2d(conv2_1, kernel)
@@ -63,18 +67,19 @@ def vgg(X):
         out = tf.nn.bias_add(conv, biases)
         conv2_2 = tf.nn.relu(out, name=scope)
 
-        # pool2
+    # pool2
     pool2 = max_pool_2x2(conv2_2, name='pool2')
+    pool2_drop = tf.nn.dropout(pool2, keep_prob)
 
     # conv3_1
     with tf.name_scope('conv3_1') as scope:
         kernel = weight_variable([3, 3, 64, 128])
-        conv = conv2d(pool2, kernel)
+        conv = conv2d(pool2_drop, kernel)
         biases = bias_variable([128])
         out = tf.nn.bias_add(conv, biases)
         conv3_1 = tf.nn.relu(out, name=scope)
 
-        # conv3_2
+    # conv3_2
     with tf.name_scope('conv3_2') as scope:
         kernel = weight_variable([3, 3, 128, 128])
         conv = conv2d(conv3_1, kernel)
@@ -82,7 +87,7 @@ def vgg(X):
         out = tf.nn.bias_add(conv, biases)
         conv3_2 = tf.nn.relu(out, name=scope)
 
-        # conv3_3
+    # conv3_3
     with tf.name_scope('conv3_3') as scope:
         kernel = weight_variable([3, 3, 128, 128])
         conv = conv2d(conv3_2, kernel)
@@ -90,33 +95,35 @@ def vgg(X):
         out = tf.nn.bias_add(conv, biases)
         conv3_3 = tf.nn.relu(out, name=scope)
 
-        # pool3
+    # pool3
     pool3 = max_pool_2x2(conv3_3, name='pool3')
+    pool3_drop = tf.nn.dropout(pool3, keep_prob)
 
-        # fc1
+    # fc1
     with tf.name_scope('fc1') as scope:
-        shape = int(np.prod(pool3.get_shape()[1:]))
-        fc1w = weight_variable([shape, 512])
-        fc1b = bias_variable([512])
-        pool3_flat = tf.reshape(pool3, [-1, shape])
+        shape = int(np.prod(pool3_drop.get_shape()[1:]))
+        fc1w = weight_variable([shape, 1024])
+        fc1b = bias_variable([1024])
+        pool3_flat = tf.reshape(pool3_drop, [-1, shape])
         fc1l = tf.nn.bias_add(tf.matmul(pool3_flat, fc1w), fc1b)
         fc1 = tf.nn.relu(fc1l)
 
-        # dropout layer after first fully connected
-    with tf.name_scope('dropout'):
-        keep_prob = tf.placeholder(tf.float32)
-        fc1_drop = tf.nn.dropout(fc1, keep_prob)
+    # dropout_2
+    fc1_drop = tf.nn.dropout(fc1, keep_prob)
 
-        # fc2
+    # fc2
     with tf.name_scope('fc2') as scope:
-        fc2w = weight_variable([512, 512])
-        fc2b = bias_variable([512])
+        fc2w = weight_variable([1024, 1024])
+        fc2b = bias_variable([1024])
         fc2l = tf.nn.bias_add(tf.matmul(fc1_drop, fc2w), fc2b)
         fc2 = tf.nn.relu(fc2l)
 
+    # dropout_2
+    fc2_drop = tf.nn.dropout(fc2, keep_prob)
+
         # fc3
     with tf.name_scope('fc3') as scope:
-        fc3w = weight_variable([512, 6])
+        fc3w = weight_variable([1024, 6])
         fc3b = bias_variable([6])
-        y_out = tf.nn.bias_add(tf.matmul(fc2, fc3w), fc3b)
+        y_out = tf.nn.bias_add(tf.matmul(fc2_drop, fc3w), fc3b)
     return y_out, keep_prob
