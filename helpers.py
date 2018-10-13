138a8670d4072b7c5d58a972b0cde42a2d6e2331
Philip Lu <philip.lu@wishabi.com>
2018-10-13 14:07:32 -0400

    Add imports

--- helpers.py
+++ helpers.py
@@ -1,7 +1,8 @@
 from __future__ import division
 import pandas as pd
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import tensorflow as tf
 
 def show_image(img):
     plt.figure()
